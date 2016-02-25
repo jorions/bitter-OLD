adb76f52aebdc02a10038a7017ecb52a148cd045
Jared Selcoe <jared.orion.selcoe@gmail.com>
2016-02-24 19:19:44 -0600

    Updated ModelFactory to create all User fields and make proper fields unique

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -13,7 +13,8 @@
 
 $factory->define(App\User::class, function (Faker\Generator $faker) {
     return [
-        'email' => $faker->email,
+        'name' => $faker->name,
+        'email' => $faker->unique()->email,
         'password' => bcrypt(str_random(10)),
         'remember_token' => str_random(10),
     ];
